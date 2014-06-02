10062558668c68162b5307a5e9cbd70feb83a78b
Bob Chatman <rchatman@rhythmnewmedia.com>
2014-06-02 14:01:44 -0700

    Adding tests for navigation.

--- test/admin.js
+++ test/admin.js
@@ -28,4 +28,55 @@ describe('Administrator User', function() {
         browser.success.should.be.true;
         browser.window.location.pathname.should.endWith("/admin/overview");
     });
+
+    describe("Menu Navigation", function() {
+        it('should have a Home link', function() {
+            should.exist(browser.link("Home"));
+        });
+
+        it('should have a Tracking link', function() {
+            should.exist(browser.link("Tracking"));
+        });
+
+        it('should have a Users link', function() {
+            should.exist(browser.link("Users"));
+        });
+
+        it('should have a Logout link', function() {
+            should.exist(browser.link("Logout"));
+        });
+
+        it('Home should return to /admin/', function(done) {
+            browser.clickLink("Home", function() {
+                browser.window.location.pathname.should.eql("/admin/");
+
+                done();
+            });
+        });
+
+        it('Tracking should return to /admin/tracking/', function(done) {
+            browser.clickLink("Tracking", function() {
+                browser.window.location.pathname.should.eql("/admin/tracking/");
+
+                done();
+            });
+        });
+
+        it('Users should return to /admin/users/', function(done) {
+            browser.clickLink("Users", function() {
+                browser.window.location.pathname.should.eql("/admin/users/");
+
+                done();
+            });
+        });
+
+        it('Logout should redirect to /', function(done) {
+            browser.clickLink("Logout", function() {
+
+                browser.window.location.pathname.should.eql("/");
+
+                done();
+            });
+        });
+    });
 });
