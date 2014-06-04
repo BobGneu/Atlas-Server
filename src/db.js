ed94674b4d2e848501f85c199483d4391ad197b7
Bob Chatman <bob.chatman@gmail.com>
2014-06-03 17:30:09 -0700

    Updating schema with email and required fields.

--- src/db.js
+++ src/db.js
@@ -3,16 +3,38 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var ApplicationModel = mongoose.model('Application', new Schema({
-	Name: String
+	Name: {
+		type: String,
+		required: true,
+		lowercase: true
+	}
 }));
 
 var UserModel = mongoose.model('User', new Schema({
-	Name: String,
-	PasswordHash: String
+	Name: {
+		type: String,
+		required: true,
+		lowercase: true
+	},
+	Email: {
+		type: String,
+		required: true,
+		lowercase: true
+	},
+	PasswordHash: {
+		type: String,
+		select: false,
+		required: true,
+		lowercase: true
+	}
 }));
 
 var TrackingDataModel = mongoose.model('TrackingData', new Schema({
-	Name: String
+	Name: {
+		type: String,
+		required: true,
+		lowercase: true
+	}
 }));
 
 mongoose.connect('mongodb://localhost/atlas');
