6724e58f2ed35db0a9ca674c33567e75494b2b31
Bob Chatman <rchatman@rhythmnewmedia.com>
2014-06-11 14:09:29 -0700

    correcting path for controllers. Also adding function to request - failed. To redirect when a request fails based on the privileges/role of the user.

--- app.js
+++ app.js
@@ -60,6 +60,13 @@ app.use(function (req, res, next) {
 	res.locals.user = req.user;
 	res.locals.isAdministrator = (typeof req.user !== 'undefined') && (typeof req.user.Role !== 'undefined') && req.user.Role === "Administrator";
 
+	req.failed = function () {
+		if (req.isAuthenticated())
+			res.redirect("back");
+		else
+			res.redirect("/login");
+	};
+
 	next();
 });
 
